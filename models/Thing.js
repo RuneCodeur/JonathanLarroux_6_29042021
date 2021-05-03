b9ef50d37462367936fc1227987fc9da1ebe5a93
Rackham <yreelfactory@gmail.com>
2021-05-03 16:26:30 +0200

    ajout des sauces dispo et résumé de sauces dispo

--- models/Thing.js
+++ models/Thing.js
@@ -1,15 +1,17 @@
 const mongoose = require('mongoose');
 
 const thingSchema = mongoose.Schema({
-  sauce: {
-  name: { type: String, required: true },
-  manufacturer: { type: String, required: true },
-  description: { type: String, required: true },
-  mainPepper: { type: String, required: true },
-  heat: { type: Number, required: true },
-
-  },
-  image: { type: String, required: true },
+    name: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    description: { type: String, required: true },
+    mainPepper: { type: String, required: true },
+    heat: { type: Number, required: true },
+    userId: { type: String, required: true },
+    imageUrl:{ type: String, required: true },
+    likes:{ type: Number, default: 0 },
+    dislikes:{ type: Number, default: 0 },
+    usersLiked:[],
+    usersDisliked:[],
 });
 
-module.exports = mongoose.model('Thing', thingSchema);
+module.exports = mongoose.model('sauce', thingSchema);
--- routes/stuff.js
+++ routes/stuff.js
@@ -5,10 +5,7 @@ const stuffCtrl = require('../controllers/stuff');
 const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
-router.post('/', auth, multer, stuffCtrl.createThing);
-router.put('/:id', auth, multer, stuffCtrl.modifyThing); 
-router.delete('/:id', auth, stuffCtrl.deleteThing);
-router.get('/:id', auth, stuffCtrl.getOneThing);
-router.get('/', auth, stuffCtrl.getAllThing);
+router.post('/', auth, multer, stuffCtrl.createSauce);
+router.get('/', auth, stuffCtrl.getAllSauces);
 
 module.exports = router;
